31d5ff1eb2842dba4f63272976b05afb8e182591
Joan Maria Talarn <jmtalarn@gmail.com>
2018-04-06 23:38:38 +0200

    fix: register serviceworker in each page

--- js/main.js
+++ js/main.js
@@ -3,20 +3,7 @@ let restaurants,
   cuisines
 var map
 var markers = []
-/***
- * Register Service worker
- */
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
