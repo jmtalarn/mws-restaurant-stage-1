8bc32e04ab765f01032c50cc7abcaff4f3277111
jmtalarn <joanmaria.talarn@idiada.com>
2018-04-11 14:31:48 +0200

    feat: added eslint to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,47 @@
 var gulp = require('gulp');
 var serve = require('gulp-serve');
 var imageResize = require('gulp-image-resize');
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
+var eslint = require('gulp-eslint');
 
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', ['generate-images','styles','serve']);
+gulp.task('default', ['generate-images','styles','lint','serve']);
 
 gulp.task('serve', serve({
     port: 8000
 }));
 
 gulp.task('generate-images', function () {
-    gulp.src("img-src/**/*.{jpg,png}")
+    gulp.src('img-src/**/*.{jpg,png}')
         .pipe(imageResize({
             width: 350
         }))
         .pipe(rename(function (path) {
-            path.basename += "-small";
+            path.basename += '-small';
         }))
-        .pipe(gulp.dest("img"));
-    gulp.src("img-src/**/*.{jpg,png}")
+        .pipe(gulp.dest('img'));
+    gulp.src('img-src/**/*.{jpg,png}')
         .pipe(imageResize({
             width: 640
         }))
         .pipe(rename(function (path) {
-            path.basename += "-medium";
+            path.basename += '-medium';
         }))
-        .pipe(gulp.dest("img"));
-    gulp.src("img-src/**/*.{jpg,png}")
-        .pipe(gulp.dest("img"))
+        .pipe(gulp.dest('img'));
+    gulp.src('img-src/**/*.{jpg,png}')
+        .pipe(gulp.dest('img'))
 });
+
+gulp.task('lint', function(){
+    return gulp.src(['**/*.js','!node_modules/**/*'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+});
+gulp.task('watch:js', function(){ gulp.watch(['!node_modules/**/*','**/*.js'], ['lint'])});
 
 gulp.task('styles', function(){
     gulp.src('sass/**/*.scss')
@@ -43,13 +52,13 @@ gulp.task('styles', function(){
 gulp.task('watch:css', function(){ gulp.watch('sass/**/*.scss', ['styles'])});
 
 // Static server
-gulp.task('watch', ['watch:css'], function() {
+gulp.task('watch', ['watch:css','watch:js'], function() {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: './'
         },
         port: 8000
     });
-    gulp.watch(["**/*.html","**/*.js","**/*.css","!node_modules/**/*"])
+    gulp.watch(['!node_modules/**/*','**/*.html','**/*.js','**/*.css'])
         .on('change', browserSync.reload);
 });
