a0912979ad08e4219bb5301a6b751b9c7adc3e63
Joan Maria Talarn Espelta <jmtalarn@gmail.com>
2018-07-01 01:01:26 +0200

    Remove the now ignored directory dist/

--- dist/js/register.js
+++
@@ -1,2 +0,0 @@
-"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("./sw.js").then(function(e){console.log("ServiceWorker registration successful with scope: ",e.scope)}).catch(function(e){console.log("ServiceWorker registration failed: ",e)})});
-//# sourceMappingURL=../maps/js/register.js.map
