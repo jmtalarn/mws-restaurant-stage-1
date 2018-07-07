624bf3df65b6f3946207daa20290231dab485f9f
Joan Maria Talarn Espelta <jmtalarn@gmail.com>
2018-07-07 03:48:34 +0200

    chore: sw.js style review

--- src/sw.js
+++ src/sw.js
@@ -1,31 +1,31 @@
-const CACHE_NAME = "mws-restaurant-cache";
+const CACHE_NAME = 'mws-restaurant-cache';
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll([
-        '/cached.html',
-        '/js/cached.js'
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll([
+                '/cached.html',
+                '/js/cached.js'
+            ]);
+        })
+    );
 });
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return fetch(event.request).then(function(response) {
-          if (event.request.method=="GET" &&
+        caches.open(CACHE_NAME).then(function(cache) {
+            return fetch(event.request).then(function(response) {
+                if (event.request.method=='GET' &&
           !(/^https:\/\/fonts\.googleapis\.com\//.test(event.request.url)) &&
           !(/^https:\/\/fonts\.gstatic\.com\//.test(event.request.url)) && 
-          event.request.url.indexOf("browser-sync/socket.io")==-1
-          ){
-          cache.put(event.request, response.clone());
+          event.request.url.indexOf('browser-sync/socket.io')==-1
+                ){
+                    cache.put(event.request, response.clone());
           
-         }
-          return response;
-        }).catch(err=>{
-            return cache.match(event.request)
-              .then( response=> (response || cache.match("/cached.html")) );
-        });
-      })
+                }
+                return response;
+            }).catch(err=>{
+                return cache.match(event.request)
+                    .then( response=> (response || cache.match('/cached.html')) );
+            });
+        })
     );
-  });
+});
